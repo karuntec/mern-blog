1c61f5c10e964710363ec3a4bb78740de663ee99
karuntec <karuntech999@gmail.com>
2024-03-21 00:02:33 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+
+
+mongoose.connect('mongodb+srv://karuntech999:jpftCWCJsVr9UVRe@cluster0.ubvyiax.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=Cluster0')
+.then(
+    ()=>{
+        console.log('mongodb is connected');
+    }
+).catch((err)=>{
+    console.log(err);
+})
+
 
 const app=express();
-
 app.listen(3000,()=>{
     console.log('server is running on port 3000!');
 });
