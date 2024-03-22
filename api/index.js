03a7010005b3cd60d03702f7b49bd08be005ef38
karuntec <karuntech999@gmail.com>
2024-03-22 21:42:42 +0530

    create a test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './route/user.route.js';
 
 dotenv.config();
 
@@ -19,3 +20,4 @@ app.listen(3000,()=>{
     console.log('server is running on port 3000!');
 });
 
+app.use('/api/user',userRoutes);
