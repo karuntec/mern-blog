d0188fb4ccf4dfd59ea44b8ab710e5f82b2ec04a
karun_alapati <124122254+karuntec@users.noreply.github.com>
2024-03-22 21:17:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-
-mongoose.connect('mongodb+srv://karuntech999:jpftCWCJsVr9UVRe@cluster0.ubvyiax.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=Cluster0')
-.then(
-    ()=>{
-        console.log('mongodb is connected');
-    }
-).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 
 const app=express();
