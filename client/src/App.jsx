4858dca989ce387f81d1657455923221205f7b44
karuntec <karuntech999@gmail.com>
2024-03-24 15:45:56 +0530

    ADD footer section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App(){
   return (
@@ -20,7 +21,7 @@ export default function App(){
 
     </Routes>
     
-    
+    <Footer/>
     </BrowserRouter>
   )
 }
